b51e5073ca33465e2ef46d41f8adc46042594c7c
kumar1397 <sahuiswar007@outlook.com>
2025-05-27 00:43:08 +0530

    4

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,11 +1,23 @@
 // firebase.js
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+import { initializeApp, credential as _credential, firestore } from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+initializeApp({
+    credential: _credential.cert(
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40digiclone-ai.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com"
+    ),
 });
 
-const db = admin.firestore();
+const db = firestore();
 
-module.exports = db;
+export default db;
